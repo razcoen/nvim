1662ba550bfc418d8dd030ed55acd51d9f932c7c
razcoen <raz@icohens.com>
2022-03-09 00:23:03 +0200

    feat: colors to the completion menu

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -16,8 +16,74 @@ vim.highlight.create('DiagnosticVirtualTextHint', { guifg = "White", ctermfg = "
 vim.highlight.create('Comment', { gui = "italic", cterm = "italic" }, false);
 
 -- Transparent background
-vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
-vim.cmd('hi LineNr guibg=NONE ctermbg=NONE')
-vim.cmd('hi SignColumn guibg=NONE ctermbg=NONE')
-vim.cmd('hi VertSplit guibg=NONE ctermbg=NONE')
-vim.cmd('hi NonText guibg=NONE ctermbg=NONE')
+local clear_group_bg = function(group, highlights)
+  if group then
+    if not highlights then
+      highlights = vim.api.nvim_exec("highlight " .. group, true)
+    end
+  else
+    if highlights then
+      group = vim.split(highlights, " ")[1]
+    else
+      return
+    end
+  end
+  if highlights:match("links to") then
+    return
+  end
+
+  local term = highlights:match([[term=([%w#]+)]]) or "NONE"
+  local ctermfg = highlights:match([[ctermfg=([%w#]+)]]) or "NONE"
+  local gui = highlights:match([[gui=([%w#]+)]]) or "NONE"
+  local guifg = highlights:match([[guifg=([%w#]+)]]) or "NONE"
+  vim.cmd(
+    string.format(
+      "hi %s term=%s ctermfg=%s ctermbg=NONE gui=%s guifg=%s guibg=NONE",
+      group,
+      term,
+      ctermfg,
+      gui,
+      guifg
+    )
+  )
+end
+
+local groups = {
+  "Normal",
+  "Comment",
+  "Constant",
+  "Special",
+  "Identifier",
+  "Statement",
+  "PreProc",
+  "Type",
+  "Underlined",
+  "Todo",
+  "String",
+  "Function",
+  "Conditional",
+  "Repeat",
+  "Operator",
+  "Structure",
+  "LineNr",
+  "NonText",
+  "SignColumn",
+  "CursorLineNr",
+  "EndOfBuffer",
+}
+
+local clear_bg = function()
+  for _, group in ipairs(groups) do
+    clear_group_bg(group)
+  end
+end
+
+clear_bg()
+
+-- completion colors
+vim.api.nvim_exec("highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6", true)
+vim.api.nvim_exec("highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6", true)
+vim.api.nvim_exec("highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0", true)
+vim.api.nvim_exec("highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0", true)
+vim.api.nvim_exec("highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE", true)
+vim.api.nvim_exec("highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4", true)
