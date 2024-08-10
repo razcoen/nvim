543c70ce8490fb7e41c385f6c0f11ccafbeed911
Raz Cohen <raz@icohens.com>
2024-08-10 22:51:56 +0300

    feat(markdown): add markdown viewer

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,5 +2,13 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    }
   }
 }
