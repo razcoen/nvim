987bd6b84f9ed75614af5c08f6695e64aa59ff1d
Raz Cohen <raz@icohens.com>
2024-08-31 15:34:33 +0300

    feat(colorscheme): going back to fleet

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,16 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
+    "razcoen/fleet.nvim",
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd([[colorscheme kanagawa-dragon]])
+      vim.cmd([[ colorscheme fleet ]])
     end,
   },
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      vim.cmd([[ TransparentEnable ]])
+    end,
+  }
 }
